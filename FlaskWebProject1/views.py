4d7832d10e26aa634911fdcd8f6b7511853e6006
Fireheart182 <cweissgl@andrew.cmu.edu>
2017-02-11 09:57:40 -0500

    checkout attempt

--- FlaskWebProject1/views.py
+++ FlaskWebProject1/views.py
@@ -5,6 +5,33 @@ Routes and views for the flask application.
 from datetime import datetime
 from flask import render_template
 from FlaskWebProject1 import app
+import sqlite3
+from flask import g, Flask, jsonify, request
+
+DATABASE = []
+class DB ():
+  def __init__(this,name,password):
+    this.name = name
+    this.password = password
+    this.skills = [[],[],[]]
+
+# END DB CLASS
+
+@app.route('/field_lookup', methods = ['POST'])
+def db_lookup():
+  name = request.form.get('id')
+  for i in range(len (DATABASE)):
+    if (DATABASE[i].name == name):
+      return jsonify(DATABASE[i].skills)
+  return jsonify(None)
+
+@app.route('/field_set', methods=['POST'])
+def db_append():
+  id = request.form.get('id')
+  password = request.form.get('password')
+  p = DB(id, password)
+  DATABASE.append(p)
+  return jsonify("Success");
 
 @app.route('/')
 @app.route('/home')
@@ -42,3 +69,14 @@ def user_tests():
     return render_template(
         'user_index.html',
     )
+"""if (__name__ == '__main__'):
+  get_db().execute(
+CREATE TABLE USERS(
+  ID INT PRIMARY KEY NOT NULL,
+  ANDREW TEXT NOT NULL,
+  PASSWORD TEXT NOT NULL,
+  CONVERSATIONAL TEXT,
+  PRACTICE TEXT,
+  TEACH TEXT
+  );)
+  app.run()"""
